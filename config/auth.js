e01a1c9138110fa82d6ed9ff81e27130cb252099
Andrew Gu <andrew@mingbogu.com>
2015-07-11 19:08:49 -0700

    remove db files

--- config/auth.js
+++
@@ -1,8 +0,0 @@
-//The data needed for Google OAuth2 authentication
-module.exports = {
-	'googleAuth': {
-		'clientID' : '397522813934-2u7db9k13f3hmmp2pket83e9fp3ufdjf.apps.googleusercontent.com',
-		'clientSecret': '4T4XV08nXZCEPv8l9nFZ6vo8',
-		'callbackURL': 'https://andrew-weebly.herokuapp.com/admin/auth/google/callback'
-	}
-}
--- config/database.js
+++
@@ -1,4 +0,0 @@
-// The URL for the Heroku MongoLab database
-module.exports = {
-	'url': 'mongodb://andrew:weebly@ds031842.mongolab.com:31842/heroku_app36371563'
-}
